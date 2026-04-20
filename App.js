1015aa2e21d80bcaef56b287fff44d24626a45dd
Rococcoon <mar.lm@protonmail.com>
2026-04-19 17:35:42 -0700

    fix: revert https session to http

--- App.js
+++ App.js
@@ -20,11 +20,11 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    maxAge: 1000 * 60 * 60 * 24,
-    secure: process.env.NODE_ENV === "production",
+    secure: false,
     httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24,
   }
-}))
+}));
 
 app.use('/', routes);
 
